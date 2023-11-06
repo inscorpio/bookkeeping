72e407b8b9a7917435001c4712d0049f2d0eb8bc
Linyx <inscorpio@icloud.com>
2023-11-07 02:57:21 +0800

    feat: add get bills by date API

--- app/api/bill/route.ts
+++ app/api/bill/route.ts
@@ -1,9 +1,10 @@
+import type { Bill } from '@prisma/client'
 import { type NextRequest, NextResponse } from 'next/server'
 import prisma from '~/prisma/db'
 
 export async function POST(req: NextRequest) {
-  const { amount, categoryId, date, note } = await req.json() as any
-  const bill = await prisma.bill.create({
+  const { amount, categoryId, date, note }: Bill = await req.json()
+  const data = await prisma.bill.create({
     data: {
       categoryId,
       amount,
@@ -11,5 +12,26 @@ export async function POST(req: NextRequest) {
       note,
     },
   })
-  return NextResponse.json({ data: bill })
+  return NextResponse.json({ data })
+}
+
+export async function GET(_: NextRequest) {
+  const groups: { date: string }[] = await prisma.$queryRaw`
+    SELECT MAX(DATE_FORMAT(date, '%Y-%m-%d')) AS date
+    FROM Bill
+    GROUP BY DATE(date)
+    ORDER BY date DESC
+  `
+  const data = []
+  for (const { date } of groups) {
+    const bills = await prisma.$queryRaw`
+      SELECT Bill.id, Bill.amount, JSON_OBJECT('id', Category.id, 'label', Category.label) AS category
+      FROM Bill
+      INNER JOIN Category ON Bill.categoryId = Category.id
+      WHERE DATE(date) = ${date}
+    `
+    data.push({ date, bills })
+  }
+
+  return NextResponse.json({ data })
 }
