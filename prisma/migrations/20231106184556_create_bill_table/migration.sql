bd602b821f15dc311bf4918ec56a18626ba54053
Linyx <inscorpio@icloud.com>
2023-11-07 02:53:58 +0800

    chore: update prisma model & reset prisma migration

--- prisma/migrations/20231106184556_create_bill_table/migration.sql
+++ prisma/migrations/20231106184556_create_bill_table/migration.sql
@@ -1,15 +1,4 @@
 -- CreateTable
-CREATE TABLE `Category` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `createAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `label` VARCHAR(191) NOT NULL,
-    `icon` VARCHAR(191) NULL,
-
-    UNIQUE INDEX `Category_label_key`(`label`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
 CREATE TABLE `Bill` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `createAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
