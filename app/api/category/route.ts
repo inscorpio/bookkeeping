2109431d97c42641721584f0330478aa50e6ed5a
Linyx <inscorpio@icloud.com>
2023-11-07 21:27:22 +0800

    feat: add zod to validate data

--- app/api/category/route.ts
+++ app/api/category/route.ts
@@ -1,7 +1,49 @@
-import { type NextRequest, NextResponse } from 'next/server'
+import type { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
+import type { NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+import { z } from 'zod'
 import prisma from '~/prisma/db'
 
+const CategoryInputSchema = z.object({
+  label: z.string({ required_error: '请输入分类名称' }).min(1, { message: '请输入分类名称' }).max(191, { message: '分类名称不能超过 191 位' }),
+})
+
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json()
+    const validation = CategoryInputSchema.safeParse(body)
+    if (!validation.success)
+      return NextResponse.json({ message: validation.error.formErrors.fieldErrors.label?.[0] }, { status: 400 })
+
+    const data = await prisma.category.create({
+      data: {
+        label: body.label,
+      },
+      select: {
+        id: true,
+        label: true,
+      },
+    })
+    return NextResponse.json({ message: '创建成功', data }, { status: 201 })
+  }
+  catch (error) {
+    const PrismaClientKnownRequestError = error as PrismaClientKnownRequestError
+    if (PrismaClientKnownRequestError.code === 'P2002')
+      return NextResponse.json({ message: '该分类已存在' }, { status: 409 })
+
+    if (error?.toString() === 'SyntaxError: Unexpected end of JSON input')
+      return NextResponse.json({ message: '解析 ResponseBody 失败' }, { status: 400 })
+
+    return NextResponse.json({ message: '未知错误', error }, { status: 500 })
+  }
+}
+
 export async function GET(_: NextRequest) {
-  const categories = await prisma.category.findMany()
-  return NextResponse.json({ data: categories })
+  const data = await prisma.category.findMany({
+    select: {
+      id: true,
+      label: true,
+    },
+  })
+  return NextResponse.json(data, { status: 200 })
 }
