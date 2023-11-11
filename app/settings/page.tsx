7284e443442970bfb5f02866e6f8b92184d73365
Linyx <inscorpio@icloud.com>
2023-11-11 21:26:59 +0800

    chore: add avatar

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,9 +1,16 @@
 import RootLayout from '~/components/RootLayout'
+import { Avatar, AvatarFallback, AvatarImage } from '~/components/ui/avatar'
 
 export default function SettingsPage() {
   return (
     <RootLayout title="设置">
-      hello Linyx
+      <div className="flex items-center gap-2">
+        <Avatar className="w-16 h-16">
+          <AvatarImage src="https://github.com/inscorpio.png" />
+          <AvatarFallback>Ins</AvatarFallback>
+        </Avatar>
+        Linyx
+      </div>
     </RootLayout>
   )
 }
