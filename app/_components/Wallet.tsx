ec2d7697702a6208dc08c47d706b13b6e72c0b5b
Linyx <inscorpio@icloud.com>
2023-11-15 18:02:12 +0800

    refactor: wallet card style

--- app/_components/Wallet.tsx
+++ app/_components/Wallet.tsx
@@ -1,8 +1,10 @@
 import Link from 'next/link'
 import Amount from '~/components/Amount'
-import { Card, CardContent, CardHeader } from '~/components/ui/card'
+import { RequestUrl } from '~/types'
+import { request } from '~/utils'
 
-export default function Wallet() {
+export default async function Wallet() {
+  const wallets = await request.get(RequestUrl.wallet) ?? []
   return (
     <>
       <h2>
@@ -10,27 +12,26 @@ export default function Wallet() {
           我的钱包
         </Link>
       </h2>
-      <Card>
-        <CardHeader>
-          <h3>
-            微信
-          </h3>
-        </CardHeader>
-        <CardContent>
-          <div className="flex">
-            - 余额：
-            <Amount>100</Amount>
+      {
+        wallets.map(wallet => (
+          <div key={wallet.id} className="py-2 px-4 shadow rounded-md">
+            <div className="flex-x-between flex-y-center pb-1 border-b border-dashed border-stone-200">
+              <h4>{wallet.name}</h4>
+              <Amount>{wallet.amount}</Amount>
+            </div>
+            <div className="flex-x-between mt-2">
+              <div className="flex">
+                今日消费：
+                <Amount>100</Amount>
+              </div>
+              <div className="flex">
+                本月消费：
+                <Amount>100</Amount>
+              </div>
+            </div>
           </div>
-          <div className="flex">
-            - 今日消费：
-            <Amount>100</Amount>
-          </div>
-          <div className="flex">
-            - 本月消费：
-            <Amount>100</Amount>
-          </div>
-        </CardContent>
-      </Card>
+        ))
+      }
     </>
   )
 }
